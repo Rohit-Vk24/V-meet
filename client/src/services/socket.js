8e6b42546642861370456afeae4784e870b18417
Rohit-Vk24 <rohitnunela.com>
2024-10-10 21:15:58 +0530

    third commit

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:4000';
+// const SOCKET_URL = 'http://localhost:4000';
+
+const SOCKET_URL = 'https://v-meet.onrender.com';
 
 const socket = io(SOCKET_URL, {
   transports: ['websocket'],
